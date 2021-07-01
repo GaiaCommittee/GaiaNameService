7dbc118ba72e27881eea9c1108acbb2ace8a6991
Vincent <rumble.vecent@outlook.com>
2021-07-01 10:20:49 +0800

    Finish name query and register functions.

---
+++ GaiaNameClient/NameClient.cs
@@ -0,0 +1,77 @@
+using System.Collections.Generic;
+using System.Linq;
+using StackExchange.Redis;
+
+namespace Gaia.NameService
+{
+    /// <summary>
+    /// Client for name service.
+    /// This client provides name query and register services.
+    /// </summary>
+    public class NameClient
+    {
+        /// <summary>
+        /// Redis server instance.
+        /// </summary>
+        private readonly IServer Server = null;
+        /// <summary>
+        /// Redis database instance.
+        /// </summary>
+        private readonly IDatabase Database = null;
+
+        /// <summary>
+        /// Construct and connect to a Redis server.
+        /// </summary>
+        /// <param name="port">Port of the Redis server.</param>
+        /// <param name="ip">IP address of the Redis server.</param>
+        public NameClient(uint port = 6379, string ip = "127.0.0.1")
+        {
+            var connection = ConnectionMultiplexer.Connect($"{ip}:{port.ToString()}");
+            if (!connection.IsConnected) return;
+            
+            Server = connection.GetServer(connection.GetEndPoints()[0]);
+            Database = connection.GetDatabase();
+        }
+
+        /// <summary>
+        /// Query all registered name.
+        /// </summary>
+        /// <remarks>It is a time consuming function.</remarks>
+        /// <returns>All valid registered names with out prefix "gaia.names/".</returns>
+        public IEnumerable<string> GetNames()
+        {
+            return Server.Keys(pattern: "gaia.names/*").Select(name => ((string) name).Substring(11));
+        }
+
+        /// <summary>
+        /// Query whether a name exists or not.
+        /// </summary>
+        /// <param name="name">Name to query.</param>
+        /// <returns>Whether this name exists or not.</returns>
+        public bool HasName(string name)
+        {
+            return Database.KeyExists($"gaia.names/{name}");
+        }
+
+        /// <summary>
+        /// Get the address text of the given name.
+        /// </summary>
+        /// <param name="name">The name to query.</param>
+        /// <returns>Address text of the given name, or empty if it does not exist.</returns>
+        public string GetAddress(string name)
+        {
+            return Database.StringGet($"gaia.names/{name}");
+        }
+
+        /// <summary>
+        /// Construct a name token and register the given name.
+        /// </summary>
+        /// <param name="name">The name to hold.</param>
+        /// <param name="address">The address of this name.</param>
+        /// <returns>Corresponding name token.</returns>
+        public NameToken HoldName(string name, string address = "")
+        {
+            return new NameToken(Server, Database, name, address);
+        }
+    }
+}
---
+++ GaiaNameClient/NameToken.cs
@@ -0,0 +1,132 @@
+using System;
+using System.Threading;
+using StackExchange.Redis;
+
+namespace Gaia.NameService
+{
+    /// <summary>
+    /// Name token represents a name resolving item, which contains a name and an address of a service.
+    /// </summary>
+    public class NameToken : IDisposable
+    {
+        /// <summary>
+        /// Redis database instance.
+        /// </summary>
+        private readonly IDatabase Database = null;
+        /// <summary>
+        /// Redis server instance.
+        /// </summary>
+        private readonly IServer Server = null;
+
+        /// <summary>
+        /// Name of this name token.
+        /// </summary>
+        public readonly string Name;
+        
+        /// <summary>
+        /// Address text property.
+        /// </summary>
+        public string Address
+        {
+            get => GetAddress();
+            set => SetAddress(value);
+        }
+
+        /// <summary>
+        /// Constructor, should be invoked by NameClient.
+        /// </summary>
+        /// <param name="server">Redis server instance.</param>
+        /// <param name="database">Redis database instance</param>
+        /// <param name="name">Name of this name token.</param>
+        /// <param name="address">Address of this name token.</param>
+        internal NameToken(IServer server, IDatabase database, string name, string address = "")
+        {
+            Name = name;
+            Server = server;
+            Database = database;
+
+            Database?.StringSet($"gaia.names/{Name}", address);
+            Database?.KeyExpire($"gaia.names/{Name}", TimeSpan.FromSeconds(3));
+        }
+
+        /// <summary>
+        /// Unregister the name and stop the background updater.
+        /// </summary>
+        public void Dispose()
+        {
+            StopBackgroundUpdater();
+            Database?.KeyDelete($"gaia.names/{Name}");
+        }
+
+        /// <summary>
+        /// Update the timestamp to keep it valid.
+        /// </summary>
+        public void Update()
+        {
+            Database?.KeyExpire($"gaia.names/{Name}", TimeSpan.FromSeconds(3));
+        }
+
+        /// <summary>
+        /// Get the address text of the corresponding name.
+        /// </summary>
+        /// <returns>Address text of the corresponding name.</returns>
+        private string GetAddress()
+        {
+            var result = Database?.StringGet($"gaia.names/{Name}");
+            if (result != null)
+            {
+                return result;
+            }
+
+            return "";
+        }
+        
+        /// <summary>
+        /// Set the address text of the corresponding name.
+        /// </summary>
+        private void SetAddress(string address)
+        {
+            Database?.StringSet($"gaia.names/{Name}", address);
+        }
+
+        /// <summary>
+        /// Life flag for background updater thread.
+        /// Updater will exit in the frame when this flag is false.
+        /// </summary>
+        private bool UpdaterFlag = false;
+        /// <summary>
+        /// Background updater thread.
+        /// </summary>
+        private Thread UpdaterThread = null;
+        
+        /// <summary>
+        /// Start the background auto update thread.
+        /// That thread will auto update this name every second.
+        /// </summary>
+        public void StartBackgroundUpdater()
+        {
+            if (UpdaterFlag) return;
+            
+            UpdaterFlag = true;
+            UpdaterThread = new Thread( delegate()
+            {
+                while (UpdaterFlag)
+                {
+                    Update();
+                    Thread.Sleep(TimeSpan.FromSeconds(1));
+                }
+            });
+            UpdaterThread.Start();
+        }
+
+        /// <summary>
+        /// Stop the background auto update thread.
+        /// </summary>
+        public void StopBackgroundUpdater()
+        {
+            if (!UpdaterFlag) return;
+            UpdaterFlag = false;
+            UpdaterThread.Join();
+        }
+    }
+}
